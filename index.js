8238145eace2bbabb6d0bf2b37a6e5c0d570693f
Kai Wells <support@kaiwells.me>
2017-10-26 19:37:15 -0700

    Change reservation filename

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/reserve", function(req, res) {
-	res.sendFile(path.join(__dirname, "public", "reservation.html"));
+	res.sendFile(path.join(__dirname, "public", "reserve.html"));
 });
 
 app.get("/tables", function(req, res) {
