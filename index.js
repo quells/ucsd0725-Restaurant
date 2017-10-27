c0e829c4285a5c055bc051d704ad584295a15a26
Natalie Loman <natalie4@gmail.com>
2017-10-26 19:25:57 -0700

    updated js file

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-var express = require("express")
-var bodyparser = require("body-parser")
-var path = require("path")
+// dependencies
+var express = require("express");
+var bodyparser = require("body-parser");
+var path = require("path");
+
+// global variable
+var reservations = [];
 
 var PORT = process.env.PORT || 3000
 
-var app = express()
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
+var app = express();
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
+// routes
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+	res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/add", function(req, res) {
+	res.sendFile(path.join(__dirname, "public", "reservation.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "public", "tables.html"));
+});
+
+// create a new reservation
+app.post("api/new", function(req, res) {
+	var newReservation = req.body;
+	newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+	console.log(newReservation);
+
+	reservations.push(newReservation);
+
+	res.json(newReservation);
+});
 
 app.listen(PORT, function() {
 	console.log(`App is listening at http://localhost:${PORT}`)
